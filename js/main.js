bda344a9d49a7e92caa8128a4c6e96bf1391a378
Anshul Gupta <hi.anshulg@gmail.com>
2020-06-16 13:13:33 -0700

    Navbar lights up as scrolled

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
-  $(window).scroll(function(event) {
-    var x = 1;
-    $(".prof").each(function(i, el) {
-      var el = $(el);
-      if (el.visible(true)) {
-        if(x % 2 == 0) {
-          if(!el.hasClass("slidefaderight")) {
-            el.addClass("slidefadeleft"); 
-          }  
-          console.log(x);
-        } else {
-          if(!el.hasClass("slidefadeleft")) {
-            el.addClass("slidefaderight"); 
-          }
-          console.log(x);
+$(window).scroll(function (event) {
+  var x = 1;
+  $(".prof").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      if (x % 2 == 0) {
+        if (!el.hasClass("slidefaderight")) {
+          el.addClass("slidefadeleft");
         }
-        x++;
-      } 
-    });
-    
+        console.log(x);
+      } else {
+        if (!el.hasClass("slidefadeleft")) {
+          el.addClass("slidefaderight");
+        }
+        console.log(x);
+      }
+      x++;
+    }
   });
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#mainNav");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
