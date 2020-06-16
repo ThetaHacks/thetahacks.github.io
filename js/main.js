6e2cbb7f0bd2c0003caf17c498f26ed4e28e7353
Andy Li <qingyuanli23@mittymonarch.com>
2020-06-16 13:56:00 -0700

    nvmd -_-

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@ $(window).scroll(function (event) {
   var prev = "animate__fadeInRight";
   $(".prof").each(function (i, el) {
     var el = $(el);
-    if (prev === "animate__fadeInLeft") {
-      if (!el.hasClass("animate__fadeInRight")) {
-        el.addClass("animate__animated");
-        el.addClass("slidefadeleft");
-        el.addClass("animate__fadeInLeft");
-        prev = "animate__fadeInRight";
-      }
-    } else {
-      if (!el.hasClass("animate__fadeInLeft")) {
-        el.addClass("animate__animated");
-        el.addClass("slidefaderight");
-        el.addClass("animate__fadeInRight");
-        prev = "animate__fadeInLeft";
+    if (el.visible(true)) {
+      if (prev === "animate__fadeInLeft") {
+        if (!el.hasClass("animate__fadeInRight")) {
+          el.addClass("animate__animated");
+          el.addClass("slidefadeleft");
+          el.addClass("animate__fadeInLeft");
+          prev = "animate__fadeInRight";
+        }
+      } else {
+        if (!el.hasClass("animate__fadeInLeft")) {
+          el.addClass("animate__animated");
+          el.addClass("slidefaderight");
+          el.addClass("animate__fadeInRight");
+          prev = "animate__fadeInLeft";
+        }
       }
     }
   });
