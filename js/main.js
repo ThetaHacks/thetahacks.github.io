5ee19d2631acfc87231dc535e197a540b10d748b
Andy Li <qingyuanli23@mittymonarch.com>
2020-06-16 13:51:10 -0700

    minor animation bug

--- js/main.js
+++ js/main.js
@@ -2,21 +2,19 @@ $(window).scroll(function (event) {
   var prev = "animate__fadeInRight";
   $(".prof").each(function (i, el) {
     var el = $(el);
-    if (el.visible(true)) {
-      if (prev === "animate__fadeInLeft") {
-        if (!el.hasClass("animate__fadeInRight")) {
-          el.addClass("animate__animated");
-          el.addClass("slidefadeleft");
-          el.addClass("animate__fadeInLeft");
-          prev = "animate__fadeInRight";
-        }
-      } else {
-        if (!el.hasClass("animate__fadeInLeft")) {
-          el.addClass("animate__animated");
-          el.addClass("slidefaderight");
-          el.addClass("animate__fadeInRight");
-          prev = "animate__fadeInLeft";
-        }
+    if (prev === "animate__fadeInLeft") {
+      if (!el.hasClass("animate__fadeInRight")) {
+        el.addClass("animate__animated");
+        el.addClass("slidefadeleft");
+        el.addClass("animate__fadeInLeft");
+        prev = "animate__fadeInRight";
+      }
+    } else {
+      if (!el.hasClass("animate__fadeInLeft")) {
+        el.addClass("animate__animated");
+        el.addClass("slidefaderight");
+        el.addClass("animate__fadeInRight");
+        prev = "animate__fadeInLeft";
       }
     }
   });
