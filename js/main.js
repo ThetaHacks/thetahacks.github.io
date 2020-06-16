25d4880f26d2bc01572357ddbf7f21edb6b583b0
Andy Li <qingyuanli23@mittymonarch.com>
2020-06-16 13:50:01 -0700

    fix animations

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 $(window).scroll(function (event) {
-  var x = 1;
+  var prev = "animate__fadeInRight";
   $(".prof").each(function (i, el) {
     var el = $(el);
     if (el.visible(true)) {
-      if (x % 2 == 0) {
+      if (prev === "animate__fadeInLeft") {
         if (!el.hasClass("animate__fadeInRight")) {
           el.addClass("animate__animated");
           el.addClass("slidefadeleft");
           el.addClass("animate__fadeInLeft");
+          prev = "animate__fadeInRight";
         }
-        console.log(x);
       } else {
         if (!el.hasClass("animate__fadeInLeft")) {
           el.addClass("animate__animated");
           el.addClass("slidefaderight");
           el.addClass("animate__fadeInRight");
+          prev = "animate__fadeInLeft";
         }
-        console.log(x);
       }
-      x++;
     }
   });
 });
