71c967d6ee21d16b04fb01d50e99eaa791384d74
Rohan Bansal <bansalrohan18@gmail.com>
2020-06-17 17:19:31 -0700

    schedule-page, this will break a lot of things

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
+
+function showSVG() {
+  var path = document.querySelector('.mainpath');
+  var length = path.getTotalLength();
+
+  path.style.transition = path.style.WebkitTransition = 'none';
+  path.style.visibility = "visible";
+
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+
+  path.getBoundingClientRect();
+
+  path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+
+  path.style.strokeDashoffset = '0';
+
+  path = document.querySelector('.arrowpath');
+  length = path.getTotalLength();
+
+  path.style.transition = path.style.WebkitTransition = 'none';
+  path.style.visibility = "visible";
+
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+
+  path.getBoundingClientRect();
+
+  path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+
+  path.style.strokeDashoffset = '0';
+
+
+}
+
+var scheduleDone = false;
+
+$.fn.isolatedScroll = function() {
+  this.bind('mousewheel DOMMouseScroll', function (e) {
+      var delta = e.wheelDelta || (e.originalEvent && e.originalEvent.wheelDelta) || -e.detail,
+          bottomOverflow = this.scrollTop + $(this).outerHeight() - this.scrollHeight >= 0,
+          topOverflow = this.scrollTop <= 0;
+
+      if ((delta < 0 && bottomOverflow) || (delta > 0 && topOverflow)) {
+          e.preventDefault();
+      }
+  });
+  return this;
+};
+
 $(window).scroll(function (event) {
   var prev = "animate__fadeInLeft";
   var x = 0;
@@ -22,6 +72,17 @@ $(window).scroll(function (event) {
       x++;
     }
   });
+
+  $(".schedule").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      if(!scheduleDone) {
+        showSVG();
+        scheduleDone = true;
+      }
+    }  
+  });
+  //$('.svg-container').isolatedScroll();
 });
 
 $(function () {
@@ -30,3 +91,69 @@ $(function () {
     $nav.addClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
+
+function shift() {
+  if(document.getElementById("day").innerHTML == "Saturday") {
+    document.getElementById("day").innerHTML = "Sunday";
+    document.getElementById("sched1").style.display = "none";
+    document.getElementById("sched2").style.display = "block";
+  } else {
+    document.getElementById("day").innerHTML = "Saturday";
+    document.getElementById("sched2").style.display = "none";
+    document.getElementById("sched1").style.display = "block";
+  }
+}
+
+positions = {
+  "9:00" : 1,
+  "10:00" : 4,
+  "11:00" : 7,
+  "12:30" : 11,
+  "7:30" : 15,
+  "9:30" : 16.5,
+  "10:30" : 18,
+  "12:00" : 19,
+  "1:30" : 20,
+  "7:00" : 24,
+  "7:30" : 25,
+  "8:00" : 28,
+  "9:35" : 33,
+  "10:15" : 38
+};
+
+function doThis() {
+  let timeEl = this.querySelector('td');
+  let time = timeEl.innerText.split(" ")[0];
+  var arrow = document.getElementsByClassName("arrow-container")[0];
+  document.getElementById("timestamp").innerText = timeEl.innerText;
+  document.getElementById("timestamp").style.left = positions[time] + "%";
+  if(positions[time] > 18) {
+    document.getElementById("daystamp").innerText = "Sunday";
+  } else {
+    document.getElementById("daystamp").innerText = "Saturday";
+  }
+  document.getElementById("daystamp").style.left = positions[time] + "%";
+  arrow.style.left = positions[time] + "%";
+}
+
+function onRowClick(tableClass) {
+  var table = document.getElementsByClassName(tableClass);
+
+
+  var rows = table[0].getElementsByTagName("tr");
+  var i;
+  for (i = 0; i < rows.length; i++) {
+      rows[i].addEventListener('click', doThis.bind(rows[i]));
+  }
+  
+  rows = table[1].getElementsByTagName("tr");
+  var x;
+  for (x = 0; x < rows.length; x++) {
+      rows[x].addEventListener('click', doThis.bind(rows[x]));
+
+  }
+};
+
+onRowClick("schedule1");
+document.getElementById("timestamp").style.left = "1%";
+document.getElementById("daystamp").style.left = "1%";
