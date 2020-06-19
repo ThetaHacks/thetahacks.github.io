7aea5d8abed8302be8e76ede9fbc678f20a872da
Andy Li <qingyuanli23@mittymonarch.com>
2020-06-18 17:19:53 -0700

    remove mobile fix for desktops

--- js/main.js
+++ js/main.js
@@ -48,37 +48,40 @@ $.fn.isolatedScroll = function () {
 };
 
 $(window).scroll(function (event) {
-  /*var prev = "animate__fadeInLeft";
-  var x = 0;
-  $(".prof").each(function (i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      if (prev === "animate__fadeInLeft") {
-        if (!el.hasClass("animate__fadeInRight")) {
-          el.addClass("animate__animated");
-          el.addClass("slidefadeleft");
-          el.addClass("animate__fadeInLeft");
-          prev = "animate__fadeInRight";
-        }
-      } else {
-        if (!el.hasClass("animate__fadeInLeft")) {
-          el.addClass("animate__animated");
-          el.addClass("slidefaderight");
-          el.addClass("animate__fadeInRight");
-          prev = "animate__fadeInLeft";
-        }
+  if (/Mobi/.test(navigator.userAgent)) {
+    $(".prof").each(function (i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("animate__animated");
+        el.addClass("slidefadeleft");
+        el.addClass("animate__fadeInLeft");
       }
-      x++;
-    }
-  });*/
-  $(".prof").each(function (i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("animate__animated");
-      el.addClass("slidefadeleft");
-      el.addClass("animate__fadeInLeft");
-    }
-  });
+    });
+  } else {
+    var prev = "animate__fadeInLeft";
+    var x = 0;
+    $(".prof").each(function (i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        if (prev === "animate__fadeInLeft") {
+          if (!el.hasClass("animate__fadeInRight")) {
+            el.addClass("animate__animated");
+            el.addClass("slidefadeleft");
+            el.addClass("animate__fadeInLeft");
+            prev = "animate__fadeInRight";
+          }
+        } else {
+          if (!el.hasClass("animate__fadeInLeft")) {
+            el.addClass("animate__animated");
+            el.addClass("slidefaderight");
+            el.addClass("animate__fadeInRight");
+            prev = "animate__fadeInLeft";
+          }
+        }
+        x++;
+      }
+    });
+  }
 
   $(".schedule").each(function (i, el) {
     var el = $(el);
