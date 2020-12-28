ab4d939aef86323fdf2d268f1e2687a1bcfa403f
AndyLi23 <andyliqy@gmail.com>
2020-12-27 19:44:56 -0800

    fix js path

--- js/infomain.js
+++ js/infomain.js
@@ -3,7 +3,7 @@
 if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
   // Special iPad css
   $("head").append(
-    '<link rel="stylesheet" type="text/css" href="css/ipad2.css">'
+    '<link rel="stylesheet" type="text/css" href="../css/ipad2.css">'
   );
   document.getElementById('banner').style.display = "block"; 
 } else {
